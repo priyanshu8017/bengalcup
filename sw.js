ea2f4764a906ef70f07b2e2de30c01354ca73a44
priyanshu8017 <102169180+priyanshu8017@users.noreply.github.com>
2025-05-11 17:28:49 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// manifest.json
+{
+  "name": "UniteWB",
+  "short_name": "UniteWB",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#111",
+  "theme_color": "#ff9900",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "logo.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "logo.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "logo.png",
+      "sizes": "1024x1024",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
